750bb4974aceb891cded964b341cfda16777b5ed
David Vieyra <52430162+RALE0@users.noreply.github.com>
2023-06-11 22:00:15 -0600

    Act5.2 Update

--- Actividad5.2.exs
+++ Actividad5.2.exs
@@ -3,12 +3,14 @@
 
 defmodule Hw.Primes do
 
+  #Funcion para sumar los numeros primos de un rango de 1 a finish
   def sum_primes(finish) do
     primes = 1..finish |> Enum.filter(&is_prime/1)
     Enum.sum(primes)
     |> IO.inspect()
   end
 
+  #Funcion para saber si un numero es primo
   defp is_prime(n) when n < 2, do: false
   defp is_prime(2), do: true
   defp is_prime(n) do
@@ -16,6 +18,7 @@ defmodule Hw.Primes do
     Enum.empty?(divisors)
   end
 
+  #Funcion para sumar los numeros primos de un rango de manera paralela
   def sum_primes_parallel(finish, num_threads) do
     divide_range(finish, num_threads)
     |> Enum.map(&Task.async(fn -> sum_primes_parallel_helper(&1) end))
@@ -24,11 +27,13 @@ defmodule Hw.Primes do
     |> IO.inspect()
   end
 
+  #Funcion para sumar los numeros primos de un rango
   defp sum_primes_parallel_helper({start, finish}) do
     primes = (start..finish) |> Enum.filter(&is_prime/1)
     Enum.sum(primes)
   end
 
+  #Funcion para dividir el rango de numeros en partes iguales
   def divide_range(finish, num_threads) do
     chunk_size = div(finish, num_threads)
     remainder = rem(finish, num_threads)
@@ -40,17 +45,25 @@ defmodule Hw.Primes do
     |> IO.inspect()
   end
 
+  #Funciones para medir el tiempo de ejecucion de los programas
+  def timed_sum_primes(finish) do
+    start_time = :timer.tc(fn -> sum_primes(finish) end)
+    {elapsed_time, _result} = start_time
+    IO.puts("Elapsed time: #{elapsed_time} microseconds")
+  end
+
+  def timed_sum_primes_parallel(finish, num_threads) do
+    start_time = :timer.tc(fn -> sum_primes_parallel(finish, num_threads) end)
+    {elapsed_time, _result} = start_time
+    IO.puts("Elapsed time: #{elapsed_time} microseconds")
+  end
+
 end
 
-# start_time = :timer.tc(fn -> Hw.Primes.sum_primes_parallel(5000000, 14) end)
-# {elapsed_time, _result} = start_time
-# IO.puts("Elapsed time: #{elapsed_time} microseconds")
-
-
-start_time = :timer.tc(fn -> Hw.Primes.sum_primes(5000000) end)
-{elapsed_time, _result} = start_time
-IO.puts("Elapsed time: #{elapsed_time} microseconds")
-
-
+# Comandos para correr el programa
 #Hw.Primes.sum_primes_parallel(5000000, 14)
 #Hw.Primes.sum_primes(5000000)
+
+# Comandos para correr el programa con el tiempo
+#Hw.Primes.timed_sum_primes_parallel(5000000, 14)
+#Hw.Primes.timed_sum_primes(5000000)
